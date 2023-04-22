39fef29378e07144524d8d9b51a76c2a4f96f590
Ugurcan06 <ugurcan_gurel@hotmail.com>
2023-04-22 17:29:37 +0300

    TrendyolClone

---
+++ app.js
@@ -0,0 +1,38 @@
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:5,
+//     responsiveClass:true,
+//     responsive:{
+//         0:{
+//             items:1,
+//             nav:true
+//         },
+//         600:{
+//             items:3,
+//             nav:false
+//         },
+//         1000:{
+//             items:5,
+//             // nav:true,
+//             loop:false
+//         }
+//     }
+// });
+$('.owl-carousel ').owlCarousel  ({
+    loop:false,
+    
+    nav:false,
+   
+    
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
